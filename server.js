b196b8c799b7af368dbc2d20ba025d4e545194cd
Juniper James <vvlpespro@gmail.com>
2022-12-21 15:17:41 -0500

    basic login functionality

---
+++ server.js
@@ -0,0 +1,54 @@
+///depends
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT;
+const session = require("express-session");
+const methodOverride= require('method-override')
+// Database Configuration
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+/////midware
+app.use(methodOverride('_method'));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+
+// Routes / Controllers
+
+
+const sessionsController = require("./controllers/sessions");
+app.use("/sessions", sessionsController);
+
+const userController = require("./controllers/users");
+app.use("/users", userController);
+
+app.get('/', (req, res) => {
+	if (req.session.currentUser) {
+		res.render('dashboard.ejs', {
+			currentUser: req.session.currentUser
+		});
+	} else {
+		res.render('index.ejs', {
+			currentUser: req.session.currentUser
+		});
+	}
+});
+// Listener
+
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
